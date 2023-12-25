5751ccc4df3d752d6f4855c14da6e44227f57443
yuto1009 <bmwb71009@gmail.com>
2023-12-25 21:43:43 +0900

    add: 本をデータベースに登録するため処理

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,20 @@ Route::get('/', function () {
  * 本を追加
  */
 Route::post('/books', function (Request $request){
-    //
+    // dd($request);
+
+    // バリデーション
+    $validator = Validator::make($request->all(), ['item_name' => 'required|max:255',]);
+
+    // Eloquentモデル（登録処理）
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = '1';
+    $books->item_amount = '1000';
+    $books->published = '2017-03-07 00:00:00';
+    $books->save();
+    return redirect('/');
+
 });
 
 /**
